f7b35bf9bf68ec028751a8ce73df9b9508a2720e
Mohit <chaudharimohit39@gmail.com>
2022-06-30 15:03:34 +0530

    While Loop

---
+++ src/controlStatements/decisionMakingStatements/loopStatement/whileLoop.java
@@ -0,0 +1,13 @@
+package controlStatements.decisionMakingStatements.loopStatement;
+
+public class whileLoop {
+    // Calculation program
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println("Printing the even numbers upto 10");
+        while(i <= 10){
+            System.out.println(i);
+            i += 2;
+        }
+    }
+}

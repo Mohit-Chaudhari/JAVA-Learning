d7ac0cb855412385955c59ecb64cee0d494c1648
Mohit <chaudharimohit39@gmail.com>
2022-06-30 17:35:02 +0530

    Main within the class

---
+++ src/objectsAndClasses/mainWithinTheClass.java
@@ -0,0 +1,14 @@
+package objectsAndClasses;
+
+// JAVA program to illustrate how to define a class and fields
+// Defining a student class
+public class mainWithinTheClass {
+    int id;
+    String name;
+
+    public static void main(String[] args){
+        mainWithinTheClass s1 = new mainWithinTheClass();
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+    }
+}

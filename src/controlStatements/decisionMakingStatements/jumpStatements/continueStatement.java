3c9622558d9bb67a3b80a305e893498476176b3a
Mohit <chaudharimohit39@gmail.com>
2022-06-30 15:46:16 +0530

    Continue Statement

---
+++ src/controlStatements/decisionMakingStatements/jumpStatements/continueStatement.java
@@ -0,0 +1,14 @@
+package controlStatements.decisionMakingStatements.jumpStatements;
+
+public class continueStatement {
+    public static void main(String[] args) {
+        for(int i=0; i<=2; i++){
+            for(int j=i; j<=5; j++) {
+                if(j == 4){
+                    continue;
+                }
+                System.out.println(j);
+            }
+        }
+    }
+}

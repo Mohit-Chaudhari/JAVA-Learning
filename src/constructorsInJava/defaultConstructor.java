660b3e2427f5c7c226cd3ae48530a61df20df476
Mohit <chaudharimohit39@gmail.com>
2022-07-01 13:38:43 +0530

    Default constructors.

---
+++ src/constructorsInJava/defaultConstructor.java
@@ -0,0 +1,11 @@
+package constructorsInJava;
+
+public class defaultConstructor {
+    defaultConstructor(){
+        System.out.println("Default constructor created...");
+    }
+
+    public static void main(String[] args){
+        defaultConstructor d = new defaultConstructor();
+    }
+}

2d95292e8ec62af6bfa22938824497a31185e377
Mohit <chaudharimohit39@gmail.com>
2022-07-01 16:12:00 +0530

    Counter with static variable.

---
+++ src/staticKeywords/counterWithStaticVariable.java
@@ -0,0 +1,16 @@
+package staticKeywords;
+
+public class counterWithStaticVariable {
+    static int count = 0;
+
+    counterWithStaticVariable() {
+        count++;
+        System.out.println(count);
+    }
+
+    public static void main(String[] args){
+        counterWithStaticVariable c1 = new counterWithStaticVariable();
+        counterWithStaticVariable c2 = new counterWithStaticVariable();
+        counterWithStaticVariable c3 = new counterWithStaticVariable();
+    }
+}

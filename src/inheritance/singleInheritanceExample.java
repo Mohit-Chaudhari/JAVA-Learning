6e603b1282ea131b2f85ffabf6be1931f6dbdd0a
Mohit <chaudharimohit39@gmail.com>
2022-07-04 11:54:39 +0530

    Single inheritance

---
+++ src/inheritance/singleInheritanceExample.java
@@ -0,0 +1,21 @@
+package inheritance;
+
+public class singleInheritanceExample {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.bark();
+        d.eat();
+    }
+}
+
+class Animal {
+    void eat() {
+        System.out.println("Eating...");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Barking");
+    }
+}
